856757e16be509c5379e58ef0c32717eb0511212
Mateusz Haligowski <mhaligowski@googlemail.com>
2012-08-31 13:33:38 +0200

    nicer display of hours

--- db/migrate/001_create_work_times.rb
+++ db/migrate/001_create_work_times.rb
@@ -1,7 +1,7 @@
 class CreateWorkTimes < ActiveRecord::Migration
   def change
     create_table :work_times do |t|
-      t.integer :user
+      t.integer :user_id
       t.datetime :start
       t.datetime :end
     end
