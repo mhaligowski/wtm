441d9899dcc8f7db19c07a3cc2b0467e29d88c5a
Mateusz Haligowski <mhaligowski@gmail.com>
2012-09-12 12:36:37 +0200

    add wtm_menu

--- init.rb
+++ init.rb
@@ -1,4 +1,5 @@
 require 'wtm_hook_listener'
+require "menu_patch"
 
 Redmine::Plugin.register :wtm do
   name 'Wtm plugin'
@@ -7,5 +8,28 @@ Redmine::Plugin.register :wtm do
   version '0.1-dev'
   author_url 'http://github.com/mhaligowski/'
   permission :work_items, { :work_items => [:index, :toggle] }, :public => false
-  menu :top_menu, :work_items, { :controller => 'work_time', :action => 'index' }, :caption => :work_time_menu_item
+  
+  menu(:wtm_menu,
+  	:work_items_menu_item,
+  	{ :controller => 'work_time', :action => 'index'},
+  	:caption => :report_time)
+
+  menu(:wtm_menu, 
+  	:work_items_report_present_menu_item, 
+  	{ :controller => 'work_time', :action => 'index'}, 
+  	:caption => :report_present)
+
+  menu(:wtm_menu,
+  	:work_items_report_time_task_menu_item,
+  	{ :controller => 'work_time', :action => 'index'},
+  	:caption => :report_time_task)
+
+  #raporty
+  menu(:top_menu, 
+  	:work_items, 
+  	{ :controller => 'work_time', :action => 'index' },
+  	:caption => :work_time_menu_item,
+ 	:if => Proc.new{
+ 		User.current.logged?
+ 		})
 end
---
+++ lib/menu_patch.rb
@@ -0,0 +1,46 @@
+require_dependency '../lib/redmine/menu_manager'
+
+# redmine only differs between project_menu and application_menu! but we want to display the
+# time_tracker submenu only if the plugin specific controllers are called
+module MenuPatch
+
+  def self.included(base)
+    base.send(:extend, ClassMethods)
+    base.send(:include, InstanceMethods)
+    base.class_eval do
+      alias_method_chain :render_main_menu, :wtm
+      alias_method_chain :display_main_menu?, :wtm
+    end
+  end
+
+  module ClassMethods
+
+    def get_menu_name(project, params={})
+      if (project && !project.new_record?)
+        :project_menu
+      else
+        if %w(work_time).include? params[:controller]
+          :wtm_menu
+        else
+          :application_menu
+        end
+      end
+    end
+
+  end
+
+  module InstanceMethods
+
+    def display_main_menu_with_wtm?(project)
+      User.current.admin? && 
+      Redmine::MenuManager.items(Redmine::MenuManager::MenuHelper.get_menu_name(project, params)).children.present?
+    end
+
+    def render_main_menu_with_wtm(project)
+      render_menu(Redmine::MenuManager::MenuHelper.get_menu_name(project, params), project)
+    end
+
+  end
+end
+
+Redmine::MenuManager::MenuHelper.send(:include, MenuPatch)
